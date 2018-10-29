9e3601d9574ba85dce60b437149f3b85739fa7d3
yicheng <yicheng.fzu@gmail.com>
2018-10-29 23:00:13 +0800

    Fix JSBridge & Sync Clash

--- ClashX/General/JSBridgeHandler.swift
+++ ClashX/General/JSBridgeHandler.swift
@@ -25,18 +25,40 @@ class JsBridgeHelper {
         }
         
         bridge.registerHandler("writeConfigWithString") {(anydata, responseCallback) in
-            if let str = anydata as? String {
-                if (FileManager.default.fileExists(atPath: kConfigFilePath)) {
-                    try? FileManager.default.removeItem(at: URL(fileURLWithPath: kConfigFilePath))
+            do {
+                if let str = anydata as? String {
+                    if (FileManager.default.fileExists(atPath: kConfigFilePath)) {
+                        try? FileManager.default.removeItem(at: URL(fileURLWithPath: kConfigFilePath))
+                    }
+                    try str.write(to: URL(fileURLWithPath: kConfigFilePath), atomically: true, encoding: .utf8)
+                } else {
+                    responseCallback?(false)
                 }
-                try? str.write(to: URL(fileURLWithPath: kConfigFilePath), atomically: true, encoding: .utf8)
+            } catch {
+                responseCallback?(false)
             }
         }
         
+        bridge.registerHandler("isSystemProxySet") {(anydata, responseCallback) in
+            responseCallback?(ConfigManager.shared.proxyPortAutoSet)
+        }
+        
         bridge.registerHandler("setSystemProxy") {(anydata, responseCallback) in
-            if let dict = anydata as? [String:Int] {
-                let success = ProxyConfigManager.setUpSystemProxy(port: dict["port"], socksPort: dict["socksPort"])
-                responseCallback?(success)
+            if let enable = anydata as? Bool {
+                ConfigManager.shared.proxyPortAutoSet = enable
+                if let config = ConfigManager.shared.currentConfig {
+                    let success:Bool
+                    if enable{
+                        success = ProxyConfigManager.setUpSystemProxy(port:  config.port,socksPort: config.socketPort)
+                    } else {
+                        success = ProxyConfigManager.setUpSystemProxy(port:  nil,socksPort: nil)
+                    }
+                    responseCallback?(success)
+                } else {
+                    responseCallback?(false)
+                }
+            } else {
+                responseCallback?(false)
             }
         }
         
@@ -50,6 +72,9 @@ class JsBridgeHelper {
         bridge.registerHandler("setPasteboard") {(anydata, responseCallback) in
             if let str = anydata as? String {
                 NSPasteboard.general.setString(str, forType: .string)
+                responseCallback?(true)
+            } else {
+                responseCallback?(false)
             }
         }
 
@@ -65,6 +90,9 @@ class JsBridgeHelper {
         bridge.registerHandler("setStartAtLogin") { (anydata, responseCallback) in
             if let enable = anydata as? Bool {
                 LaunchAtLogin.shared.isEnabled = enable
+                responseCallback?(true)
+            } else {
+                responseCallback?(false)
             }
         }
         
@@ -72,9 +100,8 @@ class JsBridgeHelper {
         // ping-pong
         bridge.registerHandler("ping"){ (anydata, responseCallback) in
             bridge.callHandler("pong")
+            responseCallback?(true)
         }
-        
-        
         return bridge
     }
 }
--- ClashX/go.mod
+++ ClashX/go.mod
@@ -1,3 +1,3 @@
 module github.com/yichengchen/clashX/ClashX
 
-require github.com/Dreamacro/clash v0.8.2-0.20181027045756-19cbe5245616
+require github.com/Dreamacro/clash v0.9.0
