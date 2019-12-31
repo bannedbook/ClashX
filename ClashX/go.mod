d6611b605721ad4a20e82a698672f5cdd4fb7450
yicheng <11733500+yichengchen@users.noreply.github.com>
2019-12-31 21:18:09 +0800

    Update

--- ClashX/go.mod
+++ ClashX/go.mod
@@ -1,7 +1,7 @@
 module github.com/yichengchen/clashX/ClashX
 
 require (
-	github.com/Dreamacro/clash v0.17.1-0.20191228144230-6ce7b6ef8357
+	github.com/Dreamacro/clash v0.17.1-0.20191231064700-50704eaeeb26
 	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
 )
 
--- install_dependency.sh
+++ install_dependency.sh
@@ -5,11 +5,8 @@ rm -f ./ClashX/Resources/Country.mmdb
 rm -rf ./ClashX/Resources/dashboard
 rm -f GeoLite2-Country.*
 echo "install mmdb"
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
-tar -zxvf GeoLite2-Country.tar.gz
-mv GeoLite2-Country_*/GeoLite2-Country.mmdb ./ClashX/Resources/Country.mmdb
-rm GeoLite2-Country.tar.gz
-rm -r GeoLite2-Country_*
+wget https://static.clash.to/GeoIP2/GeoIP2-Country.mmdb
+mv GeoIP2-Country.mmdb ./ClashX/Resources/Country.mmdb
 echo "install dashboard"
 cd ClashX/Resources
 git clone -b gh-pages https://github.com/Dreamacro/clash-dashboard.git dashboard
