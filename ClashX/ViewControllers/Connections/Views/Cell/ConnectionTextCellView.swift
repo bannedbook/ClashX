b3234a4cc7b3d487eccabce215b41633280311f8
yicheng <11733500+yichengchen@users.noreply.github.com>
2023-10-18 11:12:22 +0800

    fix: macos 14 connection dashboard overlap issue

--- ClashX/ViewControllers/Connections/Views/Cell/ConnectionTextCellView.swift
+++ ClashX/ViewControllers/Connections/Views/Cell/ConnectionTextCellView.swift
@@ -24,6 +24,7 @@ class ConnectionTextCellView: NSView, ConnectionCellProtocol {
     }
 
     func setupUI() {
+        clipsToBounds = true
         addSubview(label)
         label.font = NSFont.systemFont(ofSize: 12)
         label.makeConstraints {
